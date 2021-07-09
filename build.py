72a68943b98194b974b29f9e723e2b9e7868792e
AutumnSun1996 <q19960105@163.com>
2021-07-09 16:03:11 +0800

    feat: 更新build实现

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 import os
-import requests
 import re
 import json
+import shutil
+
+import requests
 
 
 def get_data(url, path, cache=True):
@@ -16,21 +18,34 @@ def get_data(url, path, cache=True):
     return resp.text
 
 
-text = get_data(
-    'https://wiki.biligame.com/blhx/index.php?title=MediaWiki:PN.js&action=raw&ctype=text/javascript',
-    'resources/PN.js',
-)
+def setup_ship_reload_info():
+    text = get_data(
+        'https://wiki.biligame.com/blhx/index.php?title=MediaWiki:PN.js&action=raw&ctype=text/javascript',
+        'resources/PN.js',
+    )
 
-ship_data = {}
-for sid, blvl, row, name in re.findall(
-    r'(?m)^PN(\w+)(\d):\[(.+?)\],?\s*//(.+?)_', text
-):
-    ship_data.setdefault(sid, {'name': name, 'data': {}})
-    data = ship_data[sid]['data']
-    # 15装填基础,16装填成长,17装填额外,40可强化装填,46装填改造
-    keys = [15, 16, 17, 40, 46]
-    row = row.split(',')
-    data[blvl] = [int(row[idx]) for idx in keys]
+    ship_data = {}
+    for sid, blvl, row, name in re.findall(
+        r'(?m)^PN(\w+)(\d):\[(.+?)\],?\s*//(.+?)_', text
+    ):
+        ship_data.setdefault(sid, {'name': name, 'data': {}})
+        data = ship_data[sid]['data']
+        # 15装填基础,16装填成长,17装填额外,40可强化装填,46装填改造
+        keys = [15, 16, 17, 40, 46]
+        row = row.split(',')
+        data[blvl] = [int(row[idx]) for idx in keys]
 
-with open('resources/data_reload.json', 'w', -1, 'UTF8') as f:
-    json.dump(ship_data, f, ensure_ascii=False, indent=2)
+    with open('resources/data_reload.json', 'w', -1, 'UTF8') as f:
+        json.dump(ship_data, f, ensure_ascii=False, indent=2)
+
+
+def generate_files():
+    if os.path.exists('build'):
+        shutil.rmtree('build')
+    shutil.copytree('static', 'build')
+    shutil.copy2('resources/data_reload.json', 'build/data_reload.json')
+
+
+if __name__ == '__main__':
+    setup_ship_reload_info()
+    generate_files()
