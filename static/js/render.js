367ec702e300f16bbf0ea1c623bb93cfea0e2c31
AutumnSun1996 <q19960105@163.com>
2021-07-09 17:18:44 +0800

    feat: CD展示实现

--- static/js/render.js
+++ static/js/render.js
@@ -1,127 +1,210 @@
-import * as echarts from 'echarts';
+(function (window, document) {
+    'use strict';
+    var chartDom = document.getElementById('chart');
+    var myChart = echarts.init(chartDom);
+    var option;
 
-var chartDom = document.getElementById('main');
-var myChart = echarts.init(chartDom);
-var option;
+    var data = [];
 
-var data = [];
-var dataCount = 10;
-var startTime = 0;
-var categories = ['categoryA', 'categoryB', 'categoryC'];
-
-var style = {
-    damageBuff: {
-        color: '#7b9ce1',
-    },
-    slowdownBuff: {
-        color: '#bd6d6c',
-    },
-    CV: {
-        color: '#75d874',
-    },
-    BB: {
-        color: '#e0bc78',
+    var style = {
+        damageBuff: {
+            color: '#7b9ce1',
+        },
+        slowdownBuff: {
+            color: '#bd6d6c',
+        },
+        CV: {
+            color: '#75d874',
+        },
+        BB: {
+            color: '#e0bc78',
+        }
     }
-}
-var types = ['CV', 'BB', 'damageBuff', 'slowdownBuff'];
-
-// Generate mock data
-categories.forEach(function (category, index) {
-    var baseTime = startTime;
-    for (var i = 0; i < dataCount; i++) {
-        var typeItem = types[Math.round(Math.random() * (types.length - 1))];
-        var duration = Math.round(Math.random() * 10000);
-        var endTime = baseTime + duration;
-        data.push({
-            name: `${category}-${typeItem}`,
-            value: [
-                index,
-                baseTime,
-                endTime,
-                duration
-            ],
-            itemStyle: {
-                normal: style[typeItem]
+    function loadTimestamps(tsData, config) {
+        var res = [];
+        if (tsData.type == 'fixed') {
+            var dt = tsData.offset;
+            while (dt < config.maxDuration) {
+                res.push({
+                    start: dt,
+                    end: dt + tsData.duration,
+                    duration: tsData.duration,
+                })
+                dt += tsData.cd;
             }
+        } else if (tsData.type == 'predefined') {
+            for (var item of tsData.data) {
+                res.push({
+                    start: item[0],
+                    end: item[1],
+                    duration: item[1] - item[0],
+                })
+            }
+        } else {
+            throw Error(`未知的时间轴类型${tsData.type}!`);
+        }
+        return res;
+    }
+
+    function renderItem(params, api) {
+        var categoryIndex = api.value(0);
+        var start = api.coord([api.value(1), categoryIndex]);
+        var end = api.coord([api.value(2), categoryIndex]);
+        var height = api.size([0, 1])[1] * 0.5;
+
+        var rectShape = echarts.graphic.clipRectByRect({
+            x: start[0],
+            y: start[1] - height / 2,
+            width: end[0] - start[0],
+            height: height
+        }, {
+            x: params.coordSys.x,
+            y: params.coordSys.y,
+            width: params.coordSys.width,
+            height: params.coordSys.height
         });
-        baseTime = endTime + Math.round(Math.random() * 2000) - 2000;
+
+        return rectShape && {
+            type: 'rect',
+            transition: ['shape'],
+            shape: rectShape,
+            style: api.style()
+        };
     }
-});
 
-function renderItem(params, api) {
-    var categoryIndex = api.value(0);
-    var start = api.coord([api.value(1), categoryIndex]);
-    var end = api.coord([api.value(2), categoryIndex]);
-    var height = api.size([0, 1])[1] * 0.5;
 
-    var rectShape = echarts.graphic.clipRectByRect({
-        x: start[0],
-        y: start[1] - height / 2,
-        width: end[0] - start[0],
-        height: height
-    }, {
-        x: params.coordSys.x,
-        y: params.coordSys.y,
-        width: params.coordSys.width,
-        height: params.coordSys.height
-    });
-
-    return rectShape && {
-        type: 'rect',
-        transition: ['shape'],
-        shape: rectShape,
-        style: api.style()
+    option = {
+        tooltip: {
+            formatter: function (params) {
+                return params.marker + params.name + `: ${params.value[1]}s~${params.value[2]}s`;
+            }
+        },
+        title: {
+            text: '碧蓝航线CD计算器',
+            left: 'center'
+        },
+        dataZoom: [{
+            type: 'slider',
+            filterMode: 'weakFilter',
+            showDataShadow: false,
+            top: 400,
+            labelFormatter: ''
+        }, {
+            type: 'inside',
+            filterMode: 'weakFilter'
+        }],
+        grid: {
+            height: 300
+        },
+        xAxis: {
+            min: 0,
+            scale: true,
+            axisLabel: {
+                formatter: function (val) {
+                    return parseInt(val).toFixed(2) + ' s';
+                }
+            }
+        },
+        yAxis: {
+            data: []
+        },
     };
-}
 
+    option && myChart.setOption(option);
 
-option = {
-    tooltip: {
-        formatter: function (params) {
-            return params.marker + params.name + ': ' + params.value[3] + ' ms';
-        }
-    },
-    title: {
-        text: 'Profile',
-        left: 'center'
-    },
-    dataZoom: [{
-        type: 'slider',
-        filterMode: 'weakFilter',
-        showDataShadow: false,
-        top: 400,
-        labelFormatter: ''
-    }, {
-        type: 'inside',
-        filterMode: 'weakFilter'
-    }],
-    grid: {
-        height: 300
-    },
-    xAxis: {
-        min: 0,
-        scale: true,
-        axisLabel: {
-            formatter: function (val) {
-                return parseInt(val / 1000).toFixed(2) + ' s';
+    function delayEvent(event, dt) {
+        event.start += dt;
+        event.end += dt;
+    }
+    function delayFutureEvents(events, target, dt) {
+        for (var futureEvent of events) {
+            if (target.type === futureEvent.type && target.category == futureEvent.category) {
+                delayEvent(futureEvent, dt);
             }
         }
-    },
-    yAxis: {
-        data: categories
-    },
-    series: [{
-        type: 'custom',
-        renderItem: renderItem,
-        itemStyle: {
-            opacity: 0.8
-        },
-        encode: {
-            x: [1, 2],
-            y: 0
-        },
-        data: data
-    }]
-};
+    }
+    function updateEvents(events, config) {
+        events.sort((a, b) => {
+            return a.start - b.start;
+        });
+        data = [];
+        var lastAnim = -1, delta;
+        for (let [i, event] of events.entries()) {
+            if (event.type === 'CV') {
+                delta = event.start - lastAnim;
+                if (delta < 0.6) {
+                    delayEvent(event, 0.6 - delta);
+                }
+                lastAnim = event.start;
+            } else if (event.type === 'BB') {
+                delta = event.start - lastAnim;
+                if (delta < 0.6) {
+                    delayEvent(event, 0.6 - delta);
+                    // 后续主炮发射时间将相应推迟
+                    delayFutureEvents(events.slice(i + 1), event, 0.6 - delta)
+                }
+                lastAnim = event.start;
+            }
+            if (event.start >= config.maxDuration) {
+                return;
+            }
+            if (event.end > config.maxDuration) {
+                event.end = config.maxDuration;
+            }
+            data.push({
+                name: event.name || event.type,
+                value: [
+                    event.category,
+                    event.start,
+                    event.end,
+                    event.duration,
+                ],
+                itemStyle: {
+                    normal: style[event.type]
+                }
+            });
+        }
+    }
 
-option && myChart.setOption(option);
+    myChart.setOption(option);
+    window.setChartOption = function (shipData, extraBuffData, config) {
+        var events = [];
+        var categories = [];
+        for (var [index, ship] of shipData.entries()) {
+            if (ship.type === 'CV') {
+                events.push({
+                    category: index,
+                })
+            }
+            categories.push(ship.name);
+        }
+        if (extraBuffData && extraBuffData.length > 0) {
+            var buffCategory = categories.length;
+            categories.push('Buff');
+            for (var buff of extraBuffData) {
+                for (let ts of loadTimestamps(buff.ts, config)) {
+                    events.push({
+                        name: buff.name,
+                        type: buff.type,
+                        category: buffCategory,
+                        ...ts
+                    })
+                }
+            }
+        }
+        updateEvents(events, config);
+        console.log('set data', data);
+        myChart.setOption({
+            series: [{
+                type: 'custom',
+                renderItem: renderItem,
+                itemStyle: { opacity: 0.8 },
+                encode: { x: [1, 2], y: 0 },
+                data: data
+            }],
+            yAxis: {
+                data: categories
+            },
+        });
+    }
+})(window, document);
