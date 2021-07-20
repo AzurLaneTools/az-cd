2a3cce727b6f4de38c5f97100ca5c5bba3e7df97
qiushiyang <qiushiyang@corp.netease.com>
2021-07-20 20:35:41 +0800

    feat: 修改属性名, 改为公共属性

--- utils/enums.py
+++ utils/enums.py
@@ -2,23 +2,23 @@ import yaml
 
 
 class EnumMap(object):
-    def __init__(self, name, d, d_inv) -> None:
+    def __init__(self, name, data, inv_data) -> None:
         self.name = name
-        self._data = d
-        self._inv = d_inv
+        self.data = data
+        self.inv_data = inv_data
 
     def __getitem__(self, key):
         return self._data[key]
 
     def get(self, key):
-        if key in self._data:
-            return self._data[key]
-        if key in self._inv:
-            return self._inv[key]
+        if key in self.data:
+            return self.data[key]
+        if key in self.inv_data:
+            return self.inv_data[key]
         raise KeyError(key)
 
     def inv(self, key):
-        return self._inv[key]
+        return self.inv_data[key]
 
     def __repr__(self) -> str:
         return 'EnumMap(%s)' % self.name
@@ -26,25 +26,24 @@ class EnumMap(object):
     __str__ = __repr__
 
 
-def build_map(text):
-    d = {}
-    d_inv = {}
+def build_map(name, text):
+    data = {}
+    inv_data = {}
     idx = 1
     for item in text.split(' '):
         if not item:
             continue
         aliases = item.split('=')
         for a in aliases:
-            d[a] = idx
-        d_inv[idx] = aliases[0]
-    return d, d_inv
+            data[a] = idx
+        inv_data[idx] = aliases[0]
+    return EnumMap(name, data, inv_data)
 
 
 def build_maps(data):
     g = globals()
     for key, text in data.items():
-        d, d_inv = build_map(text)
-        g[key] = EnumMap(key, d, d_inv)
+        g[key] = build_map(key, text)
 
 
 with open('resources/enum.yml', 'rb') as f:
