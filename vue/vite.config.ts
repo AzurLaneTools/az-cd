9a929bd6e05e8046093dcd39182c9bd95c6b0cbd
qiushiyang <qiushiyang@corp.netease.com>
2021-09-25 21:58:49 +0800

    更新build配置

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    terserOptions: {
+      compress: {
+        // 生产环境时移除console
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 })
