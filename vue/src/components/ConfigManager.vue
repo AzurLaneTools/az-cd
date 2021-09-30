2b395f76b853a8e133bb00654cd5b31898d21378
AutumnSun1996 <q19960105@163.com>
2021-09-30 16:33:11 +0800

    添加重置按钮

--- vue/src/components/ConfigManager.vue
+++ vue/src/components/ConfigManager.vue
@@ -1,11 +1,20 @@
 <template>
-    <n-form-item label="忽略低星装备" label-placement="left">
+    <n-form-item label="忽略低星装备" label-placement="left" :show-feedback="false">
         <n-switch v-model:value="store.state.ignoreCommonEquips"></n-switch>
+    </n-form-item>
+    <n-form-item label-placement="left" :show-feedback="false">
+        <n-button @click="resetFleetData()">重置舰队数据</n-button>
     </n-form-item>
 </template>
 
 <script lang="ts" setup>
 import store from '../utils/store';
-import { NSwitch, NFormItem } from 'naive-ui'
+import { NSwitch, NFormItem, NButton, useMessage } from 'naive-ui'
 
+const msg = useMessage();
+function resetFleetData() {
+    store.state.fleets = []
+    store.addFleet();
+    msg.success('已重置')
+}
 </script>
